6878f483918776ceab2b795d417fd1ec159cbb9b
omercelik01 <98400905+omercelik01@users.noreply.github.com>
2023-07-29 14:59:23 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,125 @@
+
+
+import random
+
+from hangman_words import word_list
+
+
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
+
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+
+from hangman_art import logo
+print(logo)
+
+
+
+print(f"Pssst, the solution is {chosen_word}.")
+
+
+display = []
+
+
+for _ in range(word_length):
+    display += "_"
+
+print(display)
+
+end_of_game = False
+
+
+while not end_of_game:
+    guess = input("Guess a letter : ").lower()
+    
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        print(f"You guessed {guess} , that's not in the word.You lose a life.")
+        lives -= 1
+        
+    if lives == 0:
+        end_of_game = True
+        print("You lose")
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+
+
+    print(stages[lives])
+    
+    
